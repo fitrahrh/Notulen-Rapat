71d3b8ca6405e8f26f4a1528d98d5dacdbf6facf
Fitrah Rahmadanu <rh.fitrah@gmail.com>
2024-08-23 08:37:07 +0700

    chore: Update dashboard index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,29 +5,30 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content">
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">@yield('title')</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="/">@yield('title')</a></li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">@yield('title')</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="/">@yield('title')</a></li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
-    <!-- Main content -->
-    <div class="content">
-        <div class="container-fluid">
-            <div id='calendar'></div>
-        </div><!-- /.container-fluid -->
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div id='calendar'></div>
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
     </div>
-    <!-- /.content -->
 </div>
 
 @endsection
